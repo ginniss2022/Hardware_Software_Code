e50399557f27e689244331b0e77dc612dd37d9c4
Gabriel Inniss <gabevaughninn@gmail.com>
2024-01-08 11:38:16 -0500

    jan 8 2024

---
+++ Gabriel_Inniss.py
@@ -0,0 +1,9 @@
+def main(): 
+    name = input("What is your name?")
+    print("We want to know if you like programming!")
+    result = input(f"Do you like programming {name}?")
+    print(f'Great! You said "{result}"?')
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()   
