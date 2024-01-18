04728bd9605839ea943b7896f78b62d70fc6e674
Gabriel Inniss <gabevaughninn@gmail.com>
2024-01-18 10:44:12 -0500

    1/18/24

---
+++ while_loop.py
@@ -0,0 +1,24 @@
+def while_loop():
+    lang_list = ["Python", "JavaScript", "PHP", "Rust", "Solidity", "Assembly"]
+    counter = 0
+    while(counter < len(lang_list)):
+        print(lang_list[counter])
+        counter += 1
+
+def for_loop():
+    lang_list = ["Python", "JavaScript", "PHP", "Rust", "Solidity", "Assembly"]
+    for lang in lang_list:
+        print(lang, end=" ")
+
+def my_msg(loop_type):
+    print("\n################")
+    print("Running", loop_type, "loop")
+    print("################")
+
+def main():
+    my_msg("for")
+    for_loop()
+    my_msg("while")
+    while_loop()
+
+main()
