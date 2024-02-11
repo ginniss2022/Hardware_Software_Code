95acbc9f561f9dae05962cc42c86a08bdc3c3596
Gabriel Inniss <gabevaughninn@gmail.com>
2024-02-11 17:48:24 -0500

    feb 11 2024

--- check_selection.py
+++ check_selection.py
@@ -2,16 +2,19 @@ def check_selection(numbers):
     hex_list = ["A","B","C","D","E","F","0", "1", "2", "3", "4","5","6","7","8","9"]
 
     for number in numbers:
-        if number.upper() not in hex_list:
-            print("Not a dexadecimal number")
-            return False
-    return True
+        if number.upper() in hex_list:
+            return True
+    print("Not a dexadecimal number")
+    return False
 
 def main():
-    good_selection = False
-    while not good_selection:
+    good_selection = True
+    while good_selection:
         selection = input("Provide a hexadecimal number:")
         good_selection = check_selection(selection)
     print("Good Job", selection, "is a hexadeimal number!!!")
 
 main()
+
+# 3.	Is it possible to modify the code to only use good_selection and remove selection? Is this a good idea?	
+# Yes it is possible, but at that point you might as well put it all into the main function. 
